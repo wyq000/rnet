b5519cc04f2a07b3a307ab2cd0f2680e9fbb1fd2
0x676e67 <gngppz@gmail.com>
2025-02-15 19:08:46 +0800

    test: Add decompress tests (#26)

---
+++ tests/compression_test.py
@@ -0,0 +1,29 @@
+import pytest
+import rnet
+
+client = rnet.Client(tls_info=True)
+
+
+@pytest.mark.asyncio
+async def test_gzip():
+    url = "https://httpbin.org/gzip"
+    response = await client.get(url)
+    text = await response.text()
+    assert text is not None
+    assert "gzipped" in text
+    
+@pytest.mark.asyncio
+async def test_deflate():
+    url = "https://httpbin.org/deflate"
+    response = await client.get(url)
+    text = await response.text()
+    assert text is not None
+    assert "deflated" in text
+    
+@pytest.mark.asyncio
+async def test_brotli():
+    url = "https://httpbin.org/brotli"
+    response = await client.get(url)
+    text = await response.text()
+    assert text is not None
+    assert "brotli" in text
