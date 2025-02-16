21a52ccbe5430757976e7611926d8d363f0a9553
0x676e67 <gngppz@gmail.com>
2025-02-16 14:40:43 +0800

    build: update manylinux build (#29)

--- .github/musl_build.sh
+++ .github/musl_build.sh
@@ -8,19 +8,15 @@ fi
 TARGET=$1
 MANYLINUX=$2
 
-BASH_IMAGE="ghcr.io/0x676e67/rust-musl-cross"
+IMAGE="ghcr.io/0x676e67/rust-musl-cross"
 VOLUME_MAPPING="-v $(pwd):/home/rust/src"
 MATURIN_CMD="maturin build --release --out dist --find-interpreter"
 
 case $TARGET in
   x86_64-unknown-linux-musl | \
-  x86_64-unknown-linux-gnu | \
   aarch64-unknown-linux-musl | \
-  aarch64-unknown-linux-gnu | \
   armv7-unknown-linux-musleabihf | \
-  armv7-unknown-linux-gnueabihf | \
-  i686-unknown-linux-musl | \
-  i686-unknown-linux-gnu)
+  i686-unknown-linux-musl)
     ;;
   *)
     echo "Unknown target: $TARGET"
@@ -28,12 +24,8 @@ case $TARGET in
     ;;
 esac
 
-if [ "$MANYLINUX" == "manylinux" ]; then
-  echo "Building for manylinux..."
-  MATURIN_CMD="maturin build --release --target $TARGET --out dist --manylinux"
-fi
-
 echo "Building for $TARGET..."
-docker run --rm $VOLUME_MAPPING $BASH_IMAGE:$TARGET /bin/bash -c "$MATURIN_CMD"
+docker pull $IMAGE:$TARGET
+docker run --rm $VOLUME_MAPPING $IMAGE:$TARGET /bin/bash -c "$MATURIN_CMD"
 
 echo "Build completed for target: $TARGET"
