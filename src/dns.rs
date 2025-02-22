0461e7745795e4600a51b35aa4cba4adc97e791a
0x676e67 <gngppz@gmail.com>
2025-02-23 00:53:56 +0800

    docs(dns): update docs

--- src/dns.rs
+++ src/dns.rs
@@ -7,10 +7,6 @@ use std::sync::{Arc, OnceLock};
 /// This function initializes a global DNS resolver using the provided lookup IP strategy.
 /// If the DNS resolver has already been initialized, it returns the existing instance.
 ///
-/// # Arguments
-///
-/// * `strategy` - An optional `LookupIpStrategy` to use for the DNS resolver.
-///
 /// # Returns
 ///
 /// A `Result` containing an `Arc` to the `HickoryDnsResolver` instance, or an error if initialization fails.
@@ -18,15 +14,6 @@ use std::sync::{Arc, OnceLock};
 /// # Errors
 ///
 /// This function returns an error if the DNS resolver fails to initialize.
-///
-/// # Examples
-///
-/// ```rust
-/// use rnet::dns::get_or_try_init;
-/// use rquest::dns::LookupIpStrategy;
-///
-/// let resolver = get_or_try_init(LookupIpStrategy::default()).unwrap();
-/// ```
 pub fn get_or_try_init() -> crate::Result<Arc<HickoryDnsResolver>> {
     static DNS_RESOLVER: OnceLock<Result<Arc<HickoryDnsResolver>, &'static str>> = OnceLock::new();
 
